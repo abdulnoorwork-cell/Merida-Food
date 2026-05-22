1e5a8ce3284b77cbf328fad99d7e45c3bbcbc34d
abdulnoorwork-cell <abdulnoorwork@gmail.com>
2026-05-22 15:01:14 +0500

    saved

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -212,7 +212,7 @@ const Dashboard = () => {
                       </div>
                       <h6 className='max-lg:hidden mx-auto text-gray-400 text-[13px]'>{new Date(blog.created_at).toDateString()}</h6>
                       <div className=' flex text-sm items-center sm:gap-2 gap-1.5 mx-auto'>
-                        <span onClick={() => { navigate(`/admin/updateblog/${blog?._id}`) }} className='lg:text-lg text-[16px] hover:scale-105 transition-all cursor-pointer text-orange-500'>
+                        <span onClick={() => { navigate(`/admin/updateblog/${blog?._id}`) }} className='lg:text-lg text-[16px] hover:scale-105 transition-all cursor-pointer text-green-500'>
                           <FaEdit />
                         </span>
                         <span onClick={() => deleteBlog(blog._id)} className='text-[22px] text-red-500 cursor-pointer'><MdDeleteOutline /></span>
