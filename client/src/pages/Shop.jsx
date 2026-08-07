33389f66300d657bac30d6685b96ea2289e2b427
abdulnoorwork-cell <abdulnoorwork@gmail.com>
2026-08-07 20:39:13 +0500

    save

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -104,7 +104,49 @@ const Shop = () => {
             {/* PRODUCTS */}
             <div className="products container mx-auto px-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {currentProducts.map((item, index) => (
-                    <ProductCard key={index} product={item} />
+                    <div
+                        key={index}
+                        className={`product_card group relative bg-[#F6F6F7] text-black overflow-hidden`}
+                    >
+                        {/* Image */}
+                        <figure onClick={() => { navigate(`/shop/${product?._id}`); scrollTo(0, 0) }} className='relative overflow-hidden w-full h-full'>
+
+                            <img
+                                src={`${product.images[0].url}`}
+                                alt={product?.name}
+                                className="w-full h-full object-cover transform group-hover:scale-115 transition duration-500"
+                            />
+                            {/* Hover Image */}
+                            {product?.images?.[1]?.url && <img
+                                src={product?.images?.[1]?.url}
+                                alt="hover"
+                                className="sm:block hidden bg-gray-100 cursor-pointer absolute top-0 left-0 h-full w-full object-contain opacity-0 group-hover:opacity-100 transition duration-300"
+                            />}
+                        </figure>
+
+                        {/* Content */}
+                        <div className="sm:p-7 p-5">
+                            <p className='bg-[#FE6A13] text-white w-fit sm:px-4 px-3 py-1.5 rounded-tr-full rounded-br-full sm:text-sm text-xs absolute top-2 left-0'>{product.category}</p>
+                            <p onClick={() => toggleWishlist(product._id)} className='bg-[#FE6A13] text-white sm:w-9 sm:h-9 w-8 h-8 cursor-pointer rounded-full mb-1 font-medium absolute top-2 right-2 flex items-center justify-center sm:text-lg text-base'>{isInWishlist(product._id) ? <FaHeart /> : <FiHeart />}</p>
+                            {/* Rating */}
+                            <div className="rating flex items-center text-yellow-500">
+                                {[...Array(5)].map((_, i) => (
+                                    <Star key={i} size={16} fill="currentColor" strokeWidth={0} />
+                                ))}
+                            </div>
+                            <p onClick={() => { navigate(`/shop/${product?._id}`); scrollTo(0, 0) }} className="xl:text-xl text-lg font-medium leading-[1.3em] mt-2 line-clamp-1">{product.name}</p>
+                            <h5 className="text-orange-500 font-semibold mt-2 xl:text-xl text-lg">
+                                {currency}.{(product.price).toLocaleString()}
+                            </h5>
+
+                            <p className="description text-gray-500 sm:text-sm text-xs mt-2 line-clamp-2" dangerouslySetInnerHTML={{ __html: product.description }}>
+                            </p>
+
+                            <button onClick={() => addToCart(product?._id, qty)} className="mt-4 cursor-pointer bg-[#FE6A13] text-white sm:px-6 px-5 py-2 sm:text-sm text-xs hover:bg-[#1A1A1A] transition duration-200 font-medium">
+                                Add To Cart
+                            </button>
+                        </div>
+                    </div>
                 ))}
             </div>
             {/* Pagination Buttons */}
